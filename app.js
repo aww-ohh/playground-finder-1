5220ef44f50dca2789e35a691d9d30bd7e624301
Alex Wong <alexwong@Alexs-MacBook-Air-2.local>
2026-05-12 23:21:43 -0700

    Default type filter to Playgrounds and reorder buttons

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function savePref(key, value) {
 
 // ---- Restore saved preferences ----
 var savedSort = loadPref('playgroundFinder.sort', ['distance', 'rating', 'reviews'], 'distance');
-var savedType = loadPref('playgroundFinder.typeFilter', ['all', 'playground', 'park'], 'all');
+var savedType = loadPref('playgroundFinder.typeFilter', ['all', 'playground', 'park'], 'playground');
 var savedRadius = loadPref('playgroundFinder.radius', ['0.5', '1', '2', '5'], '0.5');
 
 sortSelect.value = savedSort;
